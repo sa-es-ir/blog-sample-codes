8fdb3ff4592178cb75137e73eb93cdbd6ea8f8c0
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-05-26 14:01:24 +0700

    add sample service and tests

---
+++ test/ShortSnippets.UnitTest/SampleServiceTest.cs
@@ -0,0 +1,24 @@
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+
+namespace ShortSnippets.UnitTest
+{
+    public class SampleServiceTest
+    {
+        [Fact]
+        public void CallGetMessageMethod()
+        {
+            var options = Options.Create(new SampleOptions
+            {
+                Message = "Hello, World!"
+            });
+
+            var service = new SampleService(options);
+
+            var message = service.GetMessage();
+
+            message.Should().NotBeNull();
+            message.Should().Be("Hello, World!");
+        }
+    }
+}
