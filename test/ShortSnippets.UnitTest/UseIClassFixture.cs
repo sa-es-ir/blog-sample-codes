1e7805800082cdc595c6283de4e21102f313df1e
sa-es-ir <s.esmaeelinejad@gmail.com>
2024-06-16 21:43:56 +0700

    added code for ClassFixure

--- test/ShortSnippets.UnitTest/UseIClassFixture.cs
+++ test/ShortSnippets.UnitTest/UseIClassFixture.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ShortSnippets.UnitTest;
 
-namespace ShortSnippets.UnitTest
+public class UserServiceTest : IClassFixture<UserDataSource>
 {
-    internal class UseIClassFixture
+    private readonly string userId;
+
+    public UserServiceTest(UserDataSource dataSource)
     {
+        userId = dataSource.GetUserId(); // here userId is already generated in the UserDataSource class
     }
+
+    [Fact]
+    public void Test1() { /*code*/ }
+
+    [Fact]
+    public void Test2() { /*code*/ }
+
+    [Fact]
+    public void Test3() { /*code*/ }
+}
+
+public class UserDataSource
+{
+    private readonly string userId;
+
+    public UserDataSource()
+    {
+        userId = Guid.NewGuid().ToString();
+    }
+
+    public string GetUserId() => userId;
 }
